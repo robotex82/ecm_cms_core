f6745b856bfc779dac0e956e4921b1c9f5aa4588
Roberto Vasquez Angel <robotex@robotex.de>
2012-02-12 18:27:58 +0100

    Fixed double slashes in pathnames.

--- app/controllers/ecm/page_controller.rb
+++ app/controllers/ecm/page_controller.rb
@@ -4,9 +4,19 @@ module Ecm
     
     append_view_path ::Template::Resolver.instance
     
+    def render_404
+      respond_to do |format|
+        format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => nil }
+        format.xml  { head :not_found }
+        format.any  { head :not_found }
+      end
+    end
+    
     def respond
-      @page = params[:page]
-      if template = ::Template.where(:pathname => "/#{I18n.locale}/#{params[:page].split("/").reverse.drop(1).reverse.join("/")}/").where(:basename => params[:page].split("/").last).where(:locale => I18n.locale).first
+      # if template = ::Template.where(:pathname => "/#{I18n.locale}#{params[:page].split("/").reverse.drop(1).reverse.join("/")}/").where(:basename => params[:page].split("/").last).where(:locale => I18n.locale).first
+      pathname, basename = extract_names_from_params
+      
+      if template = ::Template.where(:pathname => pathname).where(:basename => basename).where(:locale => I18n.locale).first
         @meta_description = template.meta_description
         @title = template.title
         layout = template.layout unless template.layout.nil?
@@ -23,13 +33,16 @@ module Ecm
       end  
     end
     
-    def render_404
-      respond_to do |format|
-        format.html { render :file => "#{Rails.root}/public/404.html", :status => :not_found }
-        format.xml  { head :not_found }
-        format.any  { head :not_found }
+    def extract_names_from_params
+      pathname = "#{I18n.locale}/"
+      
+      subpath = params[:page].split("/").reverse.drop(1).reverse.join("/")
+      if subpath.length > 0
+        pathname << "#{subpath}"
       end
-    end
+      basename = params[:page].split("/").last
+      return pathname, basename
+    end  
 
   end
 end      
