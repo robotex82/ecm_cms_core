1b90af2b2d00f4fcbfe16b778a8f26c67cf7dcb9
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-18 19:34:29 +0200

    Updated english locales. Updated locale generator.

--- lib/generators/ecm/cms_core/locales/locales_generator.rb
+++ lib/generators/ecm/cms_core/locales/locales_generator.rb
@@ -2,13 +2,20 @@ module Ecm
   module CmsCore
     module Generators
       class LocalesGenerator < ::Rails::Generators::Base
-        source_root File.expand_path('../templates', __FILE__)
+        source_root File.expand_path('../../../../../../config/locales', __FILE__)
         desc "Installs the needed locales"
 
         
         def generate_locales
-          copy_file "ecm.cms-core.en.yml", "config/locales/ecm.cms-core.en.yml"
-          copy_file "ecm.cms-core.de.yml", "config/locales/ecm.cms-core.de.yml"
+          copy_file "ecm.cms_core.en.yml", "config/locales/ecm.cms_core.en.yml"
+          copy_file "ecm.cms_core.forms.en.yml", "config/locales/ecm.cms_core.forms.en.yml"
+          copy_file "ecm.cms_core.folder.en.yml", "config/locales/ecm.cms_core.folder.en.yml"
+          copy_file "ecm.cms_core.template.en.yml", "config/locales/ecm.cms_core.template.en.yml"
+          
+          copy_file "ecm.cms_core.de.yml", "config/locales/ecm.cms_core.de.yml"
+          copy_file "ecm.cms_core.forms.de.yml", "config/locales/ecm.cms_core.forms.de.yml"
+          copy_file "ecm.cms_core.folder.de.yml", "config/locales/ecm.cms_core.folder.de.yml"
+          copy_file "ecm.cms_core.template.de.yml", "config/locales/ecm.cms_core.template.de.yml"
         end           
       end
     end
